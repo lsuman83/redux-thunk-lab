8fb4448dc3ff572e79980e6b3447351ddb46ec7f
Sean Bordelon <sean.j.bordelon@gmail.com>
2021-03-03 12:47:26 -0500

    commit tree c51ed35bc35cc8b3dd4f3e9da34ca827d435d074

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,15 @@
 // write your CatList component here
+import React, { Component } from 'react'
+
+class CatList extends Component {
+    render(){
+        return (
+        <div>
+            {this.props.catPics.map(catPic => <img key={catPic.id} src={catPic.url} alt={catPic.id}/>)}
+        </div>
+        )
+    }
+}
+
+
+export default CatList;
